4f207055d59d6fa90cc0aca87080ae291ea2c960
Marcos Apodaca <marcosapodaca@outlook.es>
2023-11-08 17:23:49 +0100

    add more

---
+++ JavaScript-Algorithms-and-Data-Structures/ES6/13-use-destructuring-assignment-with-the-rest-parameter-to-reassign-array-elements.js
@@ -0,0 +1,9 @@
+function removeFirstTwo(list) {
+  const [,, ...shorterList] = list;
+
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
