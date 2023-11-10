a4b10024848f16fd1ac6291dbcc348b4105c9381
Marcos Apodaca <marcosapodaca@outlook.es>
2023-11-10 15:17:03 +0100

    add more ex

---
+++ JavaScript-Algorithms-and-Data-Structures/ES6/17-write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,10 @@
+// Cambia solo el código debajo de esta línea
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    this.gear = newGear;
+  }
+};
+// Cambia solo el código encima de esta línea
+bicycle.setGear(3);
+console.log(bicycle.gear);
