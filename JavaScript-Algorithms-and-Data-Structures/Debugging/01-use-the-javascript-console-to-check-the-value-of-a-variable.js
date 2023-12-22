62ce2f170ba67f82500e09372cf0cd22c5eb71ff
Marcos Apodaca <marcosapodaca@outlook.es>
2023-12-22 02:11:51 +0100

    22/12

---
+++ JavaScript-Algorithms-and-Data-Structures/Debugging/01-use-the-javascript-console-to-check-the-value-of-a-variable.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 1;
+a++;
+// Cambia solo el código debajo de esta línea
+console.log(a)
+
+let sumAB = a + b;
+console.log(sumAB);
---
+++ JavaScript-Algorithms-and-Data-Structures/Debugging/12-prevent-infinite-loops-with-a-valid-terminal-condition.js
@@ -0,0 +1,5 @@
+function myFunc() {
+  for (let i = 0; i <= 4; i++) {
+    console.log("Still going!");
+  }
+}
