4e803c6f0b102aa4d3a2037cb9a645425a74b850
Marcos Apodaca <marcosapodaca@outlook.es>
2024-01-02 02:12:50 +0100

    Update caesars-cipher.js

--- JavaScript-Algorithms-and-Data-Structures/caesars-cipher.js
+++ JavaScript-Algorithms-and-Data-Structures/caesars-cipher.js
@@ -2,7 +2,7 @@ function rot13(str) {
   return str.replace(/[A-Z]/g, (letter) => {
     const asciiCode = letter.charCodeAt(0);
     const startUppercase = 'A'.charCodeAt(0);
-    const decodedAscii = ((asciiCode - startUppercase + 13) % 26) + startUppercase;
+    const decodedAscii = ((asciiCode - startUppercase + 13) % 26) + startUppercase ;
     return String.fromCharCode(decodedAscii);
   });
 }
